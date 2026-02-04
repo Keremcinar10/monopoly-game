2d2f8e2d767980ac6970857a7fe2aaf9500b8724
Kerem <kcinar2005@gmail.com>
2026-02-05 00:45:23 +0300

    Delete lab25 directory

--- lab25/GeometricShape2D.java
+++
@@ -1,9 +0,0 @@
-package lab25;
-
-public abstract class GeometricShape2D {
-    
-    public abstract float calculateArea();
-    public abstract void printInfo();
-    public abstract String toString();
-    public abstract void editShape();
-}   
--- lab25/GeometricShape3D.java
+++
@@ -1,7 +0,0 @@
-package lab25;
-
-public abstract class GeometricShape3D extends GeometricShape2D{
-    
-    public abstract float calculateVolume();
-    
-}
